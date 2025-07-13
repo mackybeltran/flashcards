3dd3aa6afe51047cadf696086871297ac3cfbe23
macky beltran <mackybeltran1@gmail.com>
2025-07-13 10:41:05 -0400

    some cleanup

---
+++ src/app/_data/cardData.ts
@@ -0,0 +1,7 @@
+export const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+export const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+export const shapes = ['■', '▮', '▲', '◆', '●', '♥', '★', '⬮']
+
+export const colours = ['red', 'yellow', 'orange', 'green', 'blue', 'purple', 'black', 'pink'] 
